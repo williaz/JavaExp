9c4d1a0c7eefb614b2302fc4fa42080ab5d706d0
Williaz <williaz257@yahoo.com>
2016-10-13 14:09:26 -0400

    thread group max priority, interrupt, volatile

---
+++ src/main/java/thread/InterruptThreadGroup.java
@@ -0,0 +1,37 @@
+package thread;
+
+/**
+ * Created by williaz on 10/13/16.
+ */
+public class InterruptThreadGroup {
+
+    public static void main(String[] args) {
+        MyThread mt = new MyThread();
+        mt.setName("A");
+        mt.start();
+        mt = new MyThread();
+        mt.setName("B");
+        mt.start();
+        try {
+            Thread.sleep(2000); // Wait 2 seconds
+        } catch (InterruptedException e) {
+        }
+        // Interrupt all methods in the same thread group as the main
+        // thread
+        Thread.currentThread().getThreadGroup().interrupt();
+    }
+
+    private static class MyThread extends Thread {
+        public void run() {
+            synchronized ("A") {
+                System.out.println(getName() + " about to wait.");
+                try {
+                    "A".wait();
+                } catch (InterruptedException e) {
+                    System.out.println(getName() + " interrupted.");
+                }
+                System.out.println(getName() + " terminating.");
+            }
+        }
+    }
+}
---
+++ src/main/java/thread/ProdConsVolatile.java
@@ -0,0 +1,63 @@
+package thread;
+
+/**
+ * Created by williaz on 10/13/16.
+ */
+public class ProdConsVolatile {
+    private static volatile long sharedInteger=Long.MAX_VALUE-10000L;
+
+    public static void main(String[] args) {
+        new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
+
+    }
+
+
+    private static class Producer implements Runnable{
+//        private volatile long sharedInteger=0L;
+//
+//        public Producer(long sharedInteger) {
+//            this.sharedInteger = sharedInteger;
+//        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 100; i++){
+
+                try{
+                    Thread.sleep(400);
+                }catch(InterruptedException e){
+
+                }
+
+                sharedInteger++;
+            }
+        }
+    }//class
+
+
+    private static class Consumer implements  Runnable{
+//        private volatile long sharedInteger=0L;
+//
+//        public Consumer(long sharedInteger) {
+//            this.sharedInteger = sharedInteger;
+//        }
+
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 100; i++){
+
+                try{
+                    Thread.sleep(400);
+                }catch(InterruptedException e){
+
+                }
+
+                System.out.println(sharedInteger);
+            }
+        }
+    }//class
+
+
+}
