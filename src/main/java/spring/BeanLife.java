c2c5c7deee1f112ace0310625e23a165f056c3f7
Williaz <williaz257@yahoo.com>
2016-10-18 23:34:24 -0400

    TODO: Spring bean lifecycle showcase

---
+++ src/main/java/spring/BeanLife.java
@@ -0,0 +1,15 @@
+package spring;
+
+/**
+ * Created by williaz on 10/18/16.
+ * Showcase for Spring bean lifecycle
+ */
+public class BeanLife {
+    //TODO autowire constructor one bean
+    //TODO autowire setter one bean
+    //TODO implements InitializingBean, DisposableBean
+    //TODO @PostConstruct, @PreDestroy
+    //TODO @Bean(initMethod, destroyMethod)
+    //TODO implements BeanPostProcssor
+
+}
