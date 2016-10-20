93bab9f1bb4c5c05deaf829dd6f0617fea27efe5
Williaz <williaz257@yahoo.com>
2016-10-19 22:16:36 -0400

    reference type, scope, declare& initialize, order, gc, finalize(),

--- src/main/java/oca/JavaBlock.java
+++ src/main/java/oca/JavaBlock.java
@@ -1,7 +1,16 @@
+/**
+ * # package must be first non-comment
+ * Order PIC: package, import, and cla
+ */
 package oca;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by williaz on 10/18/16.
+ * # Secure: Java code runs inside the JVM. This creates a sandbox that
+ *   makes it hard for Java code to do evil things to the computer it is running on.
  * ###for the exam, your goal is to know details about the language and not have the IDE hide them for you.
  *
  * #1. classes are the basic building block. An object is a runtime instance of a class in memory.
@@ -65,11 +74,57 @@ public class JavaBlock {
 
     int d = 9; // 9 ->10
 
+
+    int x;
+    /**
+     * # Instance and class variables do not require you to initialize them.
+     *  As soon as you declare these variables, they are given a default value.
+     *  boolean - false; 0; 0.0; '\u0000'; null
+     *
+     * # A local variable is a variable defined within a method. Local variables must be initialized before use.
+     * They do not have a default value and contain garbage data until initialized.
+     * Until it is assigned a value, it cannot appear within an expression,
+     *
+     * # a method parameter. It is also local to the method.
+     *
+     * # Local variables—in scope from declaration to end of block
+     *  Instance variables—in scope from declaration until object garbage collected
+     *  Class variables—in scope from declaration until program ends
+     *
+     * @return
+     */
+    public int getOneNum(int c){
+        int a = 5;
+        int b;
+
+        if(a > 5){
+            b =5;
+            c =b * 2;
+        }else{
+            b =0;
+            c = b;
+        }
+
+        return c;
+
+    }
+
+    /**
+     * # Just keep in mind that finalize() might not get called and that it definitely won’t be called twice.
+     *   0-1
+     * @throws Throwable
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Calling finalize");
+    }
+
     /**
      * # A main() method is tht gateway between the startup of a Java process,
      * which is managed by the Java Virtual Machine (JVM), and the beginning of the programmer’s code.
      *
-     * # An array is a fi xed-size list of items that are all of the same type.
+     * # An array is a fixed-size list of items that are all of the same type.
      * @param args
      *
      * # Since 0 needs to be included in the range, Java takes it away from the positive side.
@@ -83,11 +138,37 @@ public class JavaBlock {
      *   A - 10, B 11, C 12, D 13, E 14, F 15
      * binary (digits 0–1), which uses the number 0 followed by b or B as a prefix—for example, 0b10
      *
+     * #You can declare many variables in the same declaration as long as they are all of the same type.
+     *  You can also initialize any or all of those values inline.
+     *  The shortcut to declare multiple variables in the same statement only works when they share a type.
+     *
+     *  #The name must begin with a letter or the symbol $ or _.
+     *  Subsequent characters may also be numbers.
+     *  You cannot use the same name as a Java reserved word.
+     *  ---->The exam will mostly use CamelCase common conventions for identifiers, but not always.
+     *  When you see a nonstandard identifier, be sure to check if it is legal.
+     *  $Don’t start any identifiers with $. The compiler uses this symbol for some files.
+     *
+     *  #An object sits on the heap and does not have a name.
+     *   An object cannot be assigned to another object,
+     *   nor can an object be passed to a method or returned from a method.
+     *
      */
+
+
     public static void main(String[] args) {
+
+        String _s1 = "Will", $s2 = "Is", s3 = "Best";
+        JavaBlock jb = new JavaBlock();
+
+
+        System.out.println("Scope "+ jb.getOneNum(4));
+
+
+
 //        System.out.println(args[0]);
 //        System.out.println(args[1]);
-        JavaBlock jb = new JavaBlock();
+
         System.out.println(jb.a);
         System.out.println(jb.b);
         System.out.println(jb.c);
@@ -109,13 +190,30 @@ public class JavaBlock {
 
         System.out.println(j7);
 
+        /*
+        You do need to know that System.gc() is not guaranteed to run,
+        and you should be able to recognize when objects become eligible for garbage collection.
+        ----> For eligible for garbage collection Q: Draw arrow figure
+         */
+        List<JavaBlock> l = new ArrayList<>();
+        l.add(jb);
+        System.out.println("Before calling gc(): "+ l.size());
+        jb = null;
+        System.gc();
+        System.out.println("After calling gc(): "+ l.size());
+
+
 
     }
 
 }
 
 
-
+/**
+ * # multiple classes can be defined in the same file, but only one of them is allowed to be public.
+ * The public class matches the name of the file.
+ * A file is also allowed to have neither class be public
+ */
 class AnotherClass{
 
 }
